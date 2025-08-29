461c7025a835e70404b8f6e144c95f7b2cda4809
brawlyfrog <vipservicesconstruction@gmail.com>
2025-08-29 04:32:42 -0500

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,13 @@
+import "./globals.css";
+
+export const metadata = { title: "IF Tactical" };
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0, fontFamily: "system-ui, sans-serif" }}>
+        {children}
+      </body>
+    </html>
+  );
+}
